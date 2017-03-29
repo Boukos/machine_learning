cafff507b12422357648624511c5e5e592ffbab5
papaiza <papaiza_12@hotmail.com>
2017-03-29 11:23:36 -0400

    Implement step 2 and face reconstruction of step 3

--- unsupervised/pca_and_kmeans.py
+++ unsupervised/pca_and_kmeans.py
@@ -15,77 +15,109 @@ test_ids = matfile['test_ids']
 #     plt.imshow(train_imgs[i])
 #     plt.show()
 
-b = np.mean(train_imgs)
-# plt.imshow(b)
+
+input_temp = []
+for i in range(len(train_imgs)):
+    input_temp.append(train_imgs[i].reshape(1, -1)[0])
+
+input_train = np.array(input_temp)
+
+
+def generate_b(image_set):
+    b = np.zeros(200*180)
+    for i in range(len(image_set)):
+        b += image_set[i]
+    b /= len(image_set)
+    return b
+
+b = generate_b(input_train)
+
+# plt.imshow(b.reshape((200, 180))
 # plt.show()
 
-X = 1.0/np.sqrt(len(train_imgs)) * (train_imgs - b)
+X =  1.0/np.sqrt(len(input_train))*(input_train - b)
 
-eigenvectors, eigenvalues, variance = np.linalg.svd(X, full_matrices=False)
-eigs, vecs = np.linalg.eig(np.dot(eigenvalues, eigenvalues.T))
+eigvecs, eigs, var = np.linalg.svd(X.T, full_matrices=False)
 
-# svd = np.dot(X.T, X)
-# [eigvals, eigvecs] = np.linalg.eigh(svd)
-# eigs, vecs = np.linalg.eig(np.dot(s, np.transpose(s)))
-# eigs, vecs = np.linalg.eig(s)
 
 sorted_eigs = sorted(eigs, reverse=True)
 
-# idx = np.argsort(-eigs)
-# eigenvalues = sorted(eigs, reverse=True)
-
-plt.plot(range(len(sorted_eigs)), sorted_eigs)
-plt.show()
+# plt.plot(range(len(sorted_eigs)), sorted_eigs)
+# plt.show()
 
 
 def fraction_of_variances(eigs):
     sum_eigs = np.sum(eigs)
-    dim_subspace = None
-    fracs = np.zeros(len(eigs))
-    for i in range(0, len(eigs)):
-        fracs[i] = np.sum(eigs[:i+1])/sum_eigs
-        if dim_subspace is None and fracs[i] >= 0.9:
-            dim_subspace = i
-    return fracs, dim_subspace
+    cum_sum = np.cumsum(eigs)/sum_eigs
+    dim_subspace = np.where(cum_sum >= 0.9)[0][0]
+    return cum_sum, dim_subspace
 
 frac_of_vars, dimensionality = fraction_of_variances(sorted_eigs)
-plt.plot(range(len(frac_of_vars)), frac_of_vars)
-plt.show()
+# plt.plot(range(len(frac_of_vars)), frac_of_vars)
+# plt.show()
 
 
-for i in range(10):
-    # W = np.diag(eigs)
-    x = np.dot(sorted_eigs[i].T, (train_imgs[0] - b))
-    # plt.imshow(x)
+# for i in range(10):
+
+    # plt.imshow(eigvecs[:, i].reshape((200, 180)))
     # plt.show()
 
 
 # STEP 3
 
-x = np.dot(sorted_eigs[:dimensionality], (train_imgs - b))
+W = eigvecs[:, 0: dimensionality]
+x = np.dot(W.T, (input_train - b).T)
 
-y_recon = b + np.dot(sorted_eigs[:dimensionality], x)
+y_recon = b + np.dot(x.T, W.T)
 
-error = abs(y_recon - train_imgs)**2
+error = abs(y_recon - input_train)**2
 
 sample = np.random.permutation(range(len(train_imgs)))
 
 for i in range(5):
     index = sample[i]
 
-    plt.title("Square Reconstruction Error: " + error[index])
-    plt.subplot(211)
-    plt.imshow(train_imgs[index])
-    plt.subplot(212)
-    plt.imshow(y_recon[index])
-    plt.subplot(213)
-    abs_dif = np.abs(train_imgs[index] - y_recon[index])
-    plt.imshow(abs_dif[index])
+    plt.title("Square Reconstruction Error: {}".format(sum(error[index])))
+    plt.subplot(131)
+    plt.imshow(input_train[index].reshape((200, 180)))
+    plt.subplot(132)
+    plt.imshow(y_recon[index].reshape((200, 180)))
+    plt.subplot(133)
+    abs_dif = abs(y_recon[index] - input_train[index])
+    plt.imshow(abs_dif.reshape((200, 180)))
     plt.show()
 
-plt.hist(error, bins="auto")
-plt.show()
+# plt.hist(error, bins="auto")
+# plt.show()
 
 
-
+# matfile_non = loadmat('nonface_images.mat')
+#
+# train_imgs_non = matfile['train_imgs']
+# test_imgs_non = matfile['test_imgs']
+# train_ids_non = matfile['train_ids']
+# test_ids_non = matfile['test_ids']
+#
+# x_non = np.dot(W, (train_imgs_non - b))
+# y_recon = b + np.dot(W, x_non)
+#
+# error = abs(y_recon - train_imgs)**2
+#
+# sample = np.random.permutation(range(len(train_imgs)))
+#
+# for i in range(5):
+#     index = sample[i]
+#
+#     plt.title("Square Reconstruction Error: " + error[index])
+#     plt.subplot(211)
+#     plt.imshow(train_imgs[index])
+#     plt.subplot(212)
+#     plt.imshow(y_recon[index])
+#     plt.subplot(213)
+#     abs_dif = np.abs(train_imgs[index] - y_recon[index])
+#     plt.imshow(abs_dif[index])
+#     plt.show()
+#
+# plt.hist(error, bins="auto")
+# plt.show()
 
